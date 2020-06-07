b19cf1af7bb06c6a7ccf9e47336315fc7ddd08e9
giterdun345 <46477620+giterdun345@users.noreply.github.com>
2020-06-06 21:51:44 -0400

    update

---
+++ Misc/Fireworks display automated/fireworks.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 23:13:24 2019
+
+@author: marti
+"""
+#5-8 sec
+#80--90 sec
+import pafy 
+import vlc
+from time import sleep
+
+# Wind Up
+url = "https://youtu.be/L8AayZn93wo"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 57 seconds
+sleep(57)
+
+# grucci 
+url = "https://youtu.be/4Espv7XtLMM"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 79 seconds
+sleep(80)
+
+# nysalute 
+url = "https://youtu.be/FlALBoTrDYM"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 70 seconds
+sleep(71)
+
+# burstnbloom 
+url = "https://youtu.be/DKAuKIOxUIs"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 50 seconds
+sleep(50)
+
+# strobespect
+url = "https://youtu.be/MQgScMz-3ec"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 53 seconds
+sleep(54)
+
+# fortress
+url = "https://youtu.be/kQIbZbYLpeo"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 48 seconds
+sleep(48)
+
+# popo
+url = "https://youtu.be/UgSk-KqhSc0"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 66seconds
+sleep(66)
+
+# urbanwarrior 
+url = "https://youtu.be/EMxIlFdlJBk"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 66seconds
+sleep(66)
+
+#grand finale #########################################
+
+# pyropulv 
+url = "https://youtu.be/ErCWvjIfolU"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 69 seconds
+sleep(68)
+
+# palmpyro 
+url = "https://youtu.be/U4U_Yg6_P00"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 42 seconds
+sleep(41)
+
+# johnnybgood
+url = "https://youtu.be/dKs69ectI7Q"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 25seconds
+sleep(25)
+
+# godfire
+url = "https://youtu.be/VcQwNn8tn7U"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 24seconds
+sleep(23)
+
+#voodoo 
+url = "https://youtu.be/IudJGeH-TLs"
+video = pafy.new(url)
+print(video.title)
+print(video.duration)
+best = video.getbest()
+media = vlc.MediaPlayer(best.url)
+media.play()
+# 37seconds
+sleep(35)
+
